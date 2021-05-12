d3caa9df72b99ba4bac3ca313d2e3f0d6cf65549
Nathan Gomes <nahtann@outlook.com>
2021-05-12 11:39:52 -0300

    Classe de testes adicionada

---
+++ src/aula/teste.java
@@ -0,0 +1,42 @@
+package aula;
+
+import aula.DAO.GerenciarClienteArraylistDAO;
+import aula.DAO.GerenciarClienteVectorDAO;
+import aula.modelo.modeloPessoa.Cliente;
+
+public class teste {
+    public static void main(String[] args) {
+        Cliente nathan = new Cliente("Nathan", "M", 20, "123456", "teste");
+        Cliente Aldo = new Cliente("Aldo", "M", 20, "123456", "teste");
+        Cliente Rebeca = new Cliente("Rebeca", "F", 20, "123456", "teste");
+        Cliente Maria = new Cliente("Maria", "F", 20, "123456", "teste");
+        Cliente Ricardo = new Cliente("Ricardo", "M", 20, "123456", "teste");
+
+        // GerenciarClienteArraylistDAO e GerenciarClienteVectorDAO funcionarão da mesma forma
+
+        // GerenciarClienteArraylistDAO gerenciar = new GerenciarClienteArraylistDAO();
+        GerenciarClienteVectorDAO gerenciar = new GerenciarClienteVectorDAO();
+
+        gerenciar.addCliente(nathan);
+        gerenciar.addCliente(Aldo);
+        gerenciar.addCliente(Rebeca);
+
+        gerenciar.listarClientes();
+
+        Cliente cliente = gerenciar.consultarCliente(1);
+        System.out.println(cliente.getNome());
+
+        gerenciar.addCliente(Maria);
+
+        gerenciar.listarClientes();
+
+        gerenciar.alterarCliente(Ricardo, 3);
+
+        gerenciar.listarClientes();
+
+        gerenciar.removerCliente(0);
+
+        gerenciar.listarClientes();
+
+    }
+}
