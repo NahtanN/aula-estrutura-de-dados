b63f3e799c5826d3b43632240fbef8d274de973b
Nathan Gomes <nahtann@outlook.com>
2021-05-12 11:31:35 -0300

    Parte pratica da atividade 03 adicionada

---
+++ src/aula/DAO/GerenciarClienteArraylistDAO.java
@@ -0,0 +1,74 @@
+package aula.DAO;
+
+import aula.modelo.modeloPessoa.Cliente;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class GerenciarClienteArraylistDAO {
+
+    private ArrayList<Cliente> clientes;
+
+    public GerenciarClienteArraylistDAO() {
+        this.clientes = new ArrayList<>();
+    }
+
+    public void addCliente(Cliente cliente) {
+        try {
+
+            this.clientes.add(cliente);
+            System.out.printf("Cliente %s adicionado com sucesso!\n", cliente.getNome());
+            System.out.println("");
+
+        } catch (Exception err) {
+            System.out.println("Ocorreu algum error!");
+        } finally {
+
+            Collections.sort(this.clientes, new Comparator<Cliente>() {
+                @Override
+                public int compare(Cliente c1, Cliente c2) {
+                    return c1.getNome().compareTo(c2.getNome());
+                }
+            });
+
+        }
+    }
+
+    public Cliente consultarCliente(int posicao) {
+        return this.clientes.get(posicao);
+    }
+
+    public void listarClientes() {
+        this.clientes.forEach(cliente -> {
+            System.out.println(cliente.getNome());
+        });
+
+        System.out.println("");
+    }
+
+    public void alterarCliente(Cliente cliente, int posicao) {
+        try {
+
+            this.clientes.set(posicao, cliente);
+            System.out.printf("Cliente %s inserido na posição %d \n", cliente.getNome(), posicao);
+
+        } catch(Exception err) {
+            System.out.println("Ocorreu algum error!");
+        }
+    }
+
+    public void removerCliente(int posicao) {
+        try {
+
+            Cliente cliente = this.consultarCliente(posicao);
+
+            this.clientes.remove(posicao);
+            System.out.printf("Cliente %s removido com sucesso!\n", cliente.getNome());
+
+        } catch(Exception err) {
+            System.out.println("Ocorreu algum error!");
+        }
+
+    }
+}
---
+++ src/aula/DAO/GerenciarClienteVectorDAO.java
@@ -0,0 +1,77 @@
+package aula.DAO;
+
+import aula.modelo.modeloPessoa.Cliente;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Vector;
+
+public class GerenciarClienteVectorDAO {
+
+    private Vector<Cliente> clientes;
+
+    public GerenciarClienteVectorDAO() {
+        this.clientes = new Vector<>();
+    }
+
+    public void addCliente(Cliente cliente) {
+        try {
+
+            this.clientes.add(cliente);
+            System.out.printf("Cliente %s adicionado com sucesso!\n", cliente.getNome());
+            System.out.println("");
+
+        } catch (Exception err) {
+            System.out.println("Ocorreu algum error!");
+        } finally {
+
+            // Organiza os clientes com base no nome em ordem alfabética
+            Collections.sort(this.clientes, new Comparator<Cliente>() {
+                @Override
+                public int compare(Cliente c1, Cliente c2) {
+                    return c1.getNome().compareTo(c2.getNome());
+                }
+            });
+
+        }
+    }
+
+    public Cliente consultarCliente(int posicao) {
+        return this.clientes.get(posicao);
+    }
+
+    public void listarClientes() {
+
+        this.clientes.forEach(cliente -> {
+            System.out.println(cliente.getNome());
+        });
+
+        System.out.println("");
+    }
+
+    public void alterarCliente(Cliente cliente, int posicao) {
+        try {
+
+            this.clientes.set(posicao, cliente);
+            System.out.printf("Cliente %s inserido na posição %d \n", cliente.getNome(), posicao);
+
+        } catch(Exception err) {
+            System.out.println("Ocorreu algum error!");
+        }
+    }
+
+    public void removerCliente(int posicao) {
+        try {
+
+            Cliente cliente = this.consultarCliente(posicao);
+
+            this.clientes.remove(posicao);
+            System.out.printf("Cliente %s removido com sucesso!\n", cliente.getNome());
+
+        } catch(Exception err) {
+            System.out.println("Ocorreu algum error!");
+        }
+
+    }
+}
